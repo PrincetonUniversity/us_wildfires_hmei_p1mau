b040d60efca42296a6bfec061d09f8203d911dc5
hassan-khan12305 <hk4982@princeton.edu>
2025-07-23 23:48:20 -0400

    path

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def load_county_geometries():
     if COUNTY_GEOMETRIES is None:
         try:
             # Path to the county boundaries shapefile
-            shapefile_path = "data/shapefiles/cb_2018_us_county_20m.shp"
+            shapefile_path = "data/shapefiles/county/cb_2024_us_county_5m.shp"
 
             # Read the shapefile
             gdf = gpd.read_file(shapefile_path)
